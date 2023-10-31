efc8299021cceb8fb25db1ed7f660121bc8cc18c
realrealyiu <1611440536@qq.com>
2023-10-31 22:27:47 +0800

    fixed linear interpolator

--- codebase/real_world/interpolators/linear_interpolator.py
+++ codebase/real_world/interpolators/linear_interpolator.py
@@ -26,7 +26,6 @@ class LinearInterpolator(Interpolator):
             ramp_ratio * controller_freq / policy_freq
         )  # total num steps per interpolator action
         self.inter_times = np.linspace(0, 1, int(self.total_steps))
-        self.use_delta_goal = use_delta_goal  # whether to use delta or absolute goals
         self.set_states(dim=ndim, ori=ori_interpolate)
 
     def set_states(self, dim=None, ori=None):
@@ -45,40 +44,41 @@ class LinearInterpolator(Interpolator):
         self.goal = copy.deepcopy(self.start)
 
     def set_start(self, start):
-        self.start = copy.deepcopy(start)
-        self.goal = copy.deepcopy(start)
+        assert len(start) == self.dim, f"Start dimension should be {self.dim}."
+
+        self.start = start
         self.interp_rots = None
         self.step = 0
 
     def set_goal(self, goal):
         assert len(goal) == self.dim, f"Goal dimension should be {self.dim}."
 
-        self.start = copy.deepcopy(self.goal)
         self.goal = goal
-        self.step = 0
 
     def get_interpolated_goal(self):
         super().get_interpolated_goal()
 
         x = copy.deepcopy(self.start)
+        goal = copy.deepcopy(self.goal)
 
         if self.ori_interpolate is not None:
-            goal = copy.deepcopy(self.goal)
-            if self.ori_interpolate == "euler":
-                key_rots = R.from_euler("xyz", [x, goal], degrees=False)
-            elif self.ori_interpolate == "quat":
-                key_rots = R.from_quat([x, goal])
+            key_rots = (
+                R.from_euler("xyz", [x, goal], degrees=False)
+                if self.ori_interpolate == "euler"
+                else R.from_quat([x, goal])
+            )
 
             if self.step == 0:
                 slerp = Slerp(self.times, key_rots)
                 self.interp_rots = slerp(self.inter_times)
 
-            if self.ori_interpolate == "euler":
-                x_current = self.interp_rots.as_euler("xyz", degrees=False)[self.step]
-            elif self.ori_interpolate == "quat":
-                x_current = self.interp_rots.as_quat()[self.step]
+            x_current = (
+                self.interp_rots.as_euler("xyz", degrees=False)[self.step]
+                if self.ori_interpolate == "euler"
+                else self.interp_rots.as_quat()[self.step]
+            )
         else:
-            dx = (self.goal - x) / (self.total_steps - self.step)
+            dx = (goal - x) / (self.total_steps - self.step)
             x_current = x + dx
 
         if self.step < self.total_steps:
