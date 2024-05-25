476e70d6c6c2e55f65587ce6a4d77e22ea8c26d0
chadwick <1611440536@qq.com>
2024-05-25 22:58:09 +0800

    updated eval_ros

--- eval_real_ros.py
+++ eval_real_ros.py
@@ -1,7 +1,11 @@
 import sys
+
 sys.path.append("/home/dc/mambaforge/envs/robodiff/lib/python3.9/site-packages")
-sys.path.append("/home/dc/Desktop/dp_ycw/follow_control/follow1/src/arm_control/scripts/KUKA-Controller")
+sys.path.append(
+    "/home/dc/Desktop/dp_ycw/follow_control/follow1/src/arm_control/scripts/KUKA-Controller"
+)
 import time
+import math
 import copy
 import click
 import cv2
@@ -20,9 +24,15 @@ from ipdb import set_trace
 from omegaconf import OmegaConf
 from common.precise_sleep import precise_sleep, precise_wait
 
-from codebase.diffusion_policy.diffusion_policy.workspace.base_workspace import BaseWorkspace
-from codebase.diffusion_policy.diffusion_policy.policy.base_image_policy import BaseImagePolicy
+from codebase.diffusion_policy.diffusion_policy.workspace.base_workspace import (
+    BaseWorkspace,
+)
+from codebase.diffusion_policy.diffusion_policy.policy.base_image_policy import (
+    BaseImagePolicy,
+)
 from codebase.diffusion_policy.diffusion_policy.common.pytorch_util import dict_apply
+from codebase.shared_memory.shared_memory_ring_buffer import SharedMemoryRingBuffer
+from multiprocessing.managers import SharedMemoryManager
 
 from utils.cv2_utils import get_image_transform
 from utils.real_inference_utils import get_real_obs_dict, get_real_obs_resolution
@@ -69,9 +79,12 @@ Press "S" to stop evaluation and gain control back.
     required=True,
     help="Path to checkpoint",
 )
-
 @click.option(
-    "--max_duration", "-md", default=500, help="Max duration for each epoch in seconds."
+    "--frequency",
+    "-f",
+    default=10,
+    type=int,
+    help="control frequency",
 )
 @click.option(
     "--steps_per_inference",
@@ -83,12 +96,15 @@ Press "S" to stop evaluation and gain control back.
 # @profile
 def main(
     input_path,
-    max_duration,
+    frequency,
     steps_per_inference,
 ):
-    global obs_ls, cnt, cfg, control_robot2, policy, max_steps, all_time_actions, ts, horizon
-    ts = 0
-    max_steps = max_duration
+    global obs_ring_buffer
+
+    dt = 1 / frequency
+    video_capture_fps = 30
+    max_obs_buffer_size = 30
+
     # load checkpoint
     ckpt_path = input_path
     payload = torch.load(open(ckpt_path, "rb"), map_location="cpu", pickle_module=dill)
@@ -108,22 +124,38 @@ def main(
     device = torch.device("cuda:0")
     policy.eval().to(device)
     policy.reset()
-    all_time_actions = torch.zeros([max_duration, max_duration+policy.horizon, policy.action_dim]).cuda()
+
     ## set inference params
     policy.num_inference_steps = 12  # DDIM inference iterations
     policy.n_action_steps = policy.horizon - policy.n_obs_steps + 1
-    horizon = policy.n_action_steps
 
     obs_res = get_real_obs_resolution(cfg.task.shape_meta)
     n_obs_steps = cfg.n_obs_steps
     print("n_obs_steps: ", n_obs_steps)
     print("steps_per_inference:", steps_per_inference)
-    obs_ls = list()
-    cnt = n_obs_steps
 
+    # buffer
+    shm_manager = SharedMemoryManager()
+    shm_manager.start()
+
+    examples = dict()
+    examples["mid_rgb"] = np.empty(shape=obs_res + (3,), dtype=np.uint8)
+    examples["right_rgb"] = np.empty(shape=obs_res + (3,), dtype=np.uint8)
+    examples["eef_qpos"] = np.empty(shape=(7,), dtype=np.float64)
+    examples["qpos"] = np.empty(shape=(7,), dtype=np.float64)
+    examples["timestamp"] = 0.0
+    obs_ring_buffer = SharedMemoryRingBuffer.create_from_examples(
+        shm_manager=shm_manager,
+        examples=examples,
+        get_max_k=max_obs_buffer_size,
+        get_time_budget=0.2,
+        put_desired_frequency=video_capture_fps,
+    )
+
+    # ros config
     rospy.init_node("eval_real_ros")
     eef_qpos = Subscriber("follow2_pos_back", PosCmd)
-    qpos = Subscriber("joint_information2",JointInformation)
+    qpos = Subscriber("joint_information2", JointInformation)
     mid = Subscriber("mid_camera", Image)
     right = Subscriber("right_camera", Image)
     control_robot2 = rospy.Publisher("test_right", JointControl, queue_size=10)
@@ -131,99 +163,123 @@ def main(
         [eef_qpos, qpos, mid, right], queue_size=10, slop=0.1
     )
     ats.registerCallback(callback)
+    rospy.Rate(frequency)
 
-    rospy.spin()
+    # data
+    last_data = None
+    right_control = JointControl()
 
-def callback(eef_qpos, qpos, image_mid,image_right):
-    global obs_ls, cnt, cfg, control_robot2, policy, max_steps, all_time_actions, ts, horizon
-    st=time.time()
-    en=time.time()
-    if ts < max_steps:
-        if len(obs_ls) < cnt:
-            print("Get Observation!")
-            st=time.time()
-            obs_data = dict()
-            # prepreocess low-dim info
-            obs_data["eef_qpos"] = np.array([eef_qpos.x, eef_qpos.y, eef_qpos.z, eef_qpos.roll, eef_qpos.pitch, eef_qpos.yaw, eef_qpos.gripper])
-            obs_data["qpos"] = qpos.joint_pos
+    # time
+    iter_idx = 0
+    start_delay = 1.0
+    frame_latency = 1 / 30
+    eval_t_start = time.time() + start_delay
+    t_start = time.monotonic() + start_delay
+    precise_wait(eval_t_start - frame_latency, time_func=time.time)
 
-            mid = image_mid
-            right = image_right
-            # process images observation
-            bridge = CvBridge()
-            mid = bridge.imgmsg_to_cv2(mid, "bgr8")
-            right = bridge.imgmsg_to_cv2(right, "bgr8")
-            
-            obs_data["mid"] = mid
-            obs_data["right"] = right
+    # inference loop
+    while not rospy.is_shutdown():
+        test_t_start = time.perf_counter()
+        t_cycle_end = t_start + (iter_idx + steps_per_inference) * dt
 
-            # canvas = np.zeros((480, 1280, 3), dtype=np.uint8)
+        # get observation
+        k = math.ceil(n_obs_steps * (video_capture_fps / frequency))
+        last_data = obs_ring_buffer.get(k=k, out=last_data)
+        last_timestamp = last_data["timestamp"][-1]
+        obs_align_timestamps = last_timestamp - (np.arange(n_obs_steps)[::-1] * dt)
 
-            # # 将图像复制到画布的特定位置
-            # canvas[:, :640, :] = mid
-            # canvas[:, 640:1280, :] = right
+        obs_dict = dict()
+        this_timestamps = last_data["timestamp"]
+        this_idxs = list()
+        for t in obs_align_timestamps:
+            is_before_idxs = np.nonzero(this_timestamps < t)[0]
+            this_idx = 0
+            if len(is_before_idxs) > 0:
+                this_idx = is_before_idxs[-1]
+            this_idxs.append(this_idx)
+        for key in last_data.keys():
+            obs_dict[key] = last_data[key][this_idxs]
 
-            # # 在一个窗口中显示排列后的图像
-            # cv2.imshow('Multi Camera Viewer', canvas)
-            # if cv2.waitKey(1) == ord("q"):
-            #     pass
+        obs_timestamps = obs_dict["timestamp"]
 
-            obs_ls.append(obs_data)
+        # inference
+        with torch.no_grad():
+            obs_dict_np = get_real_obs_dict(
+                env_obs=obs_dict,
+                shape_meta=cfg.task.shape_meta,
+            )
+            obs_dict = dict_apply(
+                obs_dict_np,
+                lambda x: torch.from_numpy(x).unsqueeze(0).to(torch.device("cuda:0")),
+            )
+
+            result = policy.predict_action(obs_dict)
+            action = result["action"][0].detach().to("cpu").numpy()
+
+        # preprocess action
+        action = action[:steps_per_inference, :]
+        action_timestamps = (
+            np.arange(len(action), dtype=np.float64)
+        ) * dt + obs_timestamps[-1]
+
+        action_exec_latency = 0.01
+        curr_time = time.time()
+        is_new = action_timestamps > (curr_time + action_exec_latency)
+
+        if np.sum(is_new) == 0:
+            action = action[-1]
+            action_timestamp = action_timestamp[-1]
+            next_step_idx = int(np.ceil((curr_time - eval_t_start) / dt))
+            action_timestamp = eval_t_start + (next_step_idx) * dt
+            print("Over budget", action_timestamp - curr_time)
         else:
-            print("Exe Actions")
-            obs = merge_obs_dict(obs_ls)
-            with torch.no_grad():
-                obs_dict_np = get_real_obs_dict(
-                    env_obs=obs, shape_meta=cfg.task.shape_meta
-                )
-                obs_dict = dict_apply(
-                    obs_dict_np,
-                    lambda x: torch.from_numpy(x)
-                    .unsqueeze(0)
-                    .to(torch.device("cuda:0")),
-                )
+            action = action[is_new]
+            action_timestamp = action_timestamps[is_new]
 
-                result = policy.predict_action(obs_dict)
-                action = (
-                    result["action"][0].detach()
-                )
-
-            right_control = JointControl()
-            all_time_actions[[ts], ts:ts+horizon] = action
-            # actions_for_curr_step = all_time_actions[:, ts]
-            # actions_populated = torch.all(actions_for_curr_step != 0, axis=1)
-            # actions_for_curr_step = actions_for_curr_step[actions_populated]
-            # k = 0.01
-            # exp_weights = np.exp(-k * np.arange(len(actions_for_curr_step)))
-            # exp_weights = exp_weights / exp_weights.sum()
-            # exp_weights = torch.from_numpy(exp_weights).cuda().unsqueeze(dim=1)
-            # raw_action = (actions_for_curr_step * exp_weights).sum(dim=0, keepdim=True)
-            # print(raw_action[0].to("cpu").numpy())
-            right_control.joint_pos = action[0].to("cpu").numpy()
+        # execute actions
+        for item in action:
+            right_control.joint_pos = item
             control_robot2.publish(right_control)
-            print(time.time() - st)
-            obs_ls = []
+            rospy.sleep()
+
+        precise_wait(t_cycle_end - frame_latency)
+        iter_idx += steps_per_inference
+
+        print(f"Inference Actual frequency {1/(time.perf_counter() - test_t_start)}")
 
 
-def merge_obs_dict(dict_ls):
-    eef_ls = []
-    qpos_ls = []
-    mid_ls = []
-    right_ls = []
-    for item in dict_ls:
-        eef_ls.append(item["eef_qpos"])
-        qpos_ls.append(item["qpos"])
-        mid_ls.append(item["mid"])
-        right_ls.append(item["right"])
+def callback(eef_qpos, qpos, image_mid, image_right):
+    global obs_ring_buffer
 
-    obs_dict = {
-        "eef_qpos": np.array(eef_ls),
-        "qpos": np.array(qpos_ls),
-        "mid": np.array(mid_ls),
-        "right": np.array(right_ls),
-    }
+    print("Get Observation!")
+    mid = image_mid
+    right = image_right
+    bridge = CvBridge()
+    receive_time = time.time()
 
-    return obs_dict
+    obs_data = dict()
+    obs_data["eef_qpos"] = np.array(
+        [
+            eef_qpos.x,
+            eef_qpos.y,
+            eef_qpos.z,
+            eef_qpos.roll,
+            eef_qpos.pitch,
+            eef_qpos.yaw,
+            eef_qpos.gripper,
+        ]
+    )
+    obs_data["qpos"] = qpos.joint_pos
+    # process images observation
+    mid = bridge.imgmsg_to_cv2(mid, "bgr8")
+    right = bridge.imgmsg_to_cv2(right, "bgr8")
+    obs_data["mid_rgb"] = mid
+    obs_data["right_rgb"] = right
+    obs_data["timestamp"] = receive_time
+
+    put_data = obs_data
+    obs_ring_buffer.put(put_data, wait=False)
+
 
 if __name__ == "__main__":
     main()
